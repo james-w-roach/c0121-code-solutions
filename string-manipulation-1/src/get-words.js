94bb498629ca487b9d0842ed562d85b89de5949f
James Roach <roachjweston@gmail.com>
2021-01-29 17:57:25 -0800

    in progress

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,16 +1,9 @@
 /* exported getWords */
 function getWords(string) {
-  var array = [];
-  var currentString = '';
-  var i = 0;
-  while (i < string.length) {
-    if (string[i] === ' ') {
-      array.push(currentString);
-      i++;
-    } else {
-      currentString += string[i];
-      i++;
-    }
+  if (string.length > 2) {
+    var array = string.split(' ');
+  } else {
+    array = string.split();
   }
   return array;
 }
