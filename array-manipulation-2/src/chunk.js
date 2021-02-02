2052f3d07f1dccfd442dff5d41b3953164253547
James Roach <roachjweston@gmail.com>
2021-02-01 22:22:50 -0800

    still in progress

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -3,13 +3,13 @@ function chunk(array, size) {
   var array2 = [];
   var subArray = [];
   var i = 0;
-  while (i < array.length) {
-    if (i < size) {
+  while (i <= array.length) {
+    if (subArray.length !== size) {
       subArray.push(array[i]);
-    } else if (i >= size) {
-      subArray.push(array[i]);
-    } else if (subArray.length === size) {
+    } else {
       array2.push(subArray);
+      subArray = [];
+      subArray.push(array[i]);
     }
     i++;
   }
