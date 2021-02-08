9d0cbeb2d140b15ed26974fede5fd2130995a7d7
James Roach <roachjweston@gmail.com>
2021-02-07 23:30:33 -0800

    in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,11 @@
 /* exported pick */
+function pick(source, keys) {
+  var object2 = {};
+  for (var i = 0; i < source.length; i++) {
+    if (source[i] === keys) {
+      object2 += source[i];
+    }
+    i++;
+  }
+  return object2;
+}
