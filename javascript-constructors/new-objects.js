bdf8d7937f1ea6a15c1c124c6a4332013ba0b475
James Roach <roachjweston@gmail.com>
2021-02-16 13:26:20 -0800

    completed javascript constructors

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,23 @@
+var object = new Object();
+console.log('value of object:', object);
+console.log('typeof object:', typeof object);
+
+var array = new Array();
+console.log('value of array:', array);
+console.log('typeof array:', typeof array);
+
+var aFunction = new Function();
+console.log('value of aFunction:', aFunction);
+console.log('typeof aFunction:', typeof aFunction);
+
+var string = new String();
+console.log('value of string:', string);
+console.log('typeof string:', typeof string);
+
+var number = new Number();
+console.log('value of number:', number);
+console.log('typeof number:', typeof number);
+
+var boolean = new Boolean();
+console.log('value of boolean:', boolean);
+console.log('typeof boolean:', typeof boolean);
