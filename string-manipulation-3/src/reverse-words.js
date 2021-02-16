21be74042c38f3520856b5b10508f1afe9799c6c
James Roach <roachjweston@gmail.com>
2021-02-15 16:41:42 -0800

    in progress

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,19 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  var string2 = '';
+  var string3 = '';
+  for (var i = 0; i <= string.length; i++) {
+    if (string[i] === ' ' || string.length === i) {
+      for (var j = (string2.length - 1); j >= 0; j--) {
+        string3 += string2[j];
+      }
+      if (i !== string.length) {
+        string3 += ' ';
+      }
+      string2 = '';
+    } else {
+      string2 += string[i];
+    }
+  }
+  return string3;
+}
