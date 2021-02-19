c85820ae4e29167a81fe1c9e77df899fe615d12a
James Roach <roachjweston@gmail.com>
2021-02-19 10:40:22 -0800

    in progress

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,23 +1,8 @@
 /* exported equal */
 function equal(first, second) {
-  var index = [];
-  var match = '';
-  if (first === second) {
+  if (JSON.stringify(first) === JSON.stringify(second)) {
     return true;
+  } else {
+    return false;
   }
-  for (var i = 0; i < first.length; i++) {
-    for (var j = 0; j < second.length; j++) {
-      if (first[i] === second[j]) {
-        index.push(j);
-      }
-    }
-  }
-  for (var k = 0; k < index.length; k++) {
-    if (k === (index.length - 1)) {
-      match += first[k];
-    } else {
-      match += first[k] + ', ';
-    }
-  }
-  return match;
 }
--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,6 @@
 /* exported flatten */
+function flatten(array) {
+  var array2 = [];
+  var new1 = JSON.stringify(array);
+  array2.push(new1);
+}
