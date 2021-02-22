6753129cd5f0711ffb774b08a55bcf1909745fd9
James Roach <roachjweston@gmail.com>
2021-02-21 19:11:26 -0800

    completed typing tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,7 +1,17 @@
-var chars = document.querySelector('.hi');
+var chars = document.querySelector('h1');
+
+var i = 0;
+
+chars.children[0].className = 'current';
 
 function type(event) {
-  chars.className = 'correct';
+  if (chars.children[i].textContent !== event.key) {
+    chars.children[i].className = 'incorrect current';
+  } else {
+    chars.children[i].className = 'correct';
+    chars.children[i + 1].className = 'current';
+    i++;
+  }
 }
 
-chars.addEventListener('keydown', type);
+window.addEventListener('keydown', type);
