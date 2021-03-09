93511e67b045b4b8ebcd54a2d96d625f49a1bbf2
James Roach <roachjweston@gmail.com>
2021-03-08 16:33:32 -0800

    in progress

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,3 +1,22 @@
 /* exported titleCase */
 function titleCase(string) {
+  var titleString = '';
+  var currentWord = '';
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      currentWord += string[i];
+    } else {
+      if (currentWord.length <= 3) {
+        titleString += currentWord;
+        currentWord = '';
+      } else {
+        titleString += currentWord[0].toUpperCase();
+        for (var j = 1; j < currentWord.length; j++) {
+          titleString += currentWord[j];
+        }
+        currentWord = '';
+      }
+    }
+  }
+  return titleString;
 }
