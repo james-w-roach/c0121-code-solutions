22062b2c1a48162ffe0e0b951bc62d2638062448
James Roach <roachjweston@gmail.com>
2021-03-10 02:26:42 +0000

    removed magic numbers and shortened repetitive code

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,68 +1,58 @@
 var $box = document.querySelector('.box');
 var $buttons = document.querySelector('.dot-buttons');
 var $images = document.querySelector('.img-container');
-
 var imgKids = $images.children;
 var buttonKids = $buttons.children;
 
 var i = 0;
 
+function determineI() {
+  if (event.target.className === 'far fa-circle') {
+    i = event.target.getAttribute('id');
+  } else if (event.target.getAttribute('id') === 'left') {
+    if (i !== 0) {
+      i--;
+    } else {
+      i = (buttonKids.length - 1);
+    }
+  } else if (event.target.getAttribute('id') === 'right') {
+    if (i !== (buttonKids.length - 1)) {
+      i++;
+    } else {
+      i = 0;
+    }
+  }
+}
+
+function changeImage() {
+  determineI();
+  for (var j = 0; j < buttonKids.length; j++) {
+    if (buttonKids[j] !== buttonKids[i]) {
+      imgKids[j].className = 'image hidden';
+      buttonKids[j].className = 'far fa-circle';
+    }
+  }
+  imgKids[i].className = 'image';
+  buttonKids[i].className = 'fas fa-circle';
+  clearInterval(id);
+  id = setInterval(imageTimer, 3000);
+}
+
+
 function imageTimer() {
-  if (i < 4) {
+  if (i < (buttonKids.length - 1)) {
     i++;
     imgKids[i - 1].className = 'image hidden';
     buttonKids[i - 1].className = 'far fa-circle';
   } else {
     i = 0;
-    imgKids[4].className = 'image hidden';
-    buttonKids[4].className = 'far fa-circle';
+    imgKids[buttonKids.length - 1].className = 'image hidden';
+    buttonKids[buttonKids.length - 1].className = 'far fa-circle';
   }
   imgKids[i].className = 'image';
   buttonKids[i].className = 'fas fa-circle';
 }
 
 var id = setInterval(imageTimer, 3000);
-
-function changeImage(event) {
-  if (event.target.getAttribute('id') === 'left') {
-    i--;
-    if (i < 0) {
-      i = 4;
-      imgKids[0].className = 'image hidden';
-      buttonKids[0].className = 'far fa-circle';
-    } else {
-      imgKids[i + 1].className = 'image hidden';
-      buttonKids[i + 1].className = 'far fa-circle';
-    }
-    imgKids[i].className = 'image';
-    buttonKids[i].className = 'fas fa-circle';
-  } else if (event.target.getAttribute('id') === 'right') {
-    i++;
-    if (i > 4) {
-      i = 0;
-      imgKids[4].className = 'image hidden';
-      buttonKids[4].className = 'far fa-circle';
-    } else {
-      imgKids[i - 1].className = 'image hidden';
-      buttonKids[i - 1].className = 'far fa-circle';
-    }
-    imgKids[i].className = 'image';
-    buttonKids[i].className = 'fas fa-circle';
-  } else {
-    if (event.target.className === 'far fa-circle') {
-      for (var j = 0; j < buttonKids.length; j++) {
-        if (buttonKids[j].className === 'fas fa-circle') {
-          buttonKids[j].className = 'far fa-circle';
-          imgKids[j].className = 'image hidden';
-        }
-      }
-      i = event.target.getAttribute('id');
-      imgKids[i].className = 'image';
-      buttonKids[i].className = 'fas fa-circle';
-    }
-  }
-  clearInterval(id);
-  id = setInterval(imageTimer, 3000);
-}
 
 $box.addEventListener('click', changeImage);
