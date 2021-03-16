2c2a676abed1bf8d5c4abb2360458bb6880670b0
James Roach <roachjweston@gmail.com>
2021-03-16 22:21:21 +0000

    completed express post json

---
+++ express-post-json/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('listening on port 3000');
+});
+
+let nextId = 1;
+
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const output = [];
+  for (const key in grades) {
+    output.push(grades[key]);
+  }
+  res.json(output);
+});
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  req.body.id = nextId;
+  grades[nextId] = req.body;
+  res.json(grades[nextId]);
+  nextId++;
+});
