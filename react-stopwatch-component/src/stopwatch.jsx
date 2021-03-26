5e029e4b0c4690c18ee9e7c36852ba29ae16e344
James Roach <roachjweston@gmail.com>
2021-03-26 20:27:01 +0000

    completed react stopwatch

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -9,43 +9,57 @@ class Stopwatch extends React.Component {
     };
     this.handlePlay = this.handlePlay.bind(this);
     this.increment = this.increment.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+    this.determineButton = this.determineButton.bind(this);
   }
 
   increment() {
-    setInterval(() => this.state.timer + 1, 1000);
+    this.setState({
+      timer: this.state.timer + 1
+    });
   }
 
   handlePlay() {
     if (this.state.isPaused) {
+      this.timerId = setInterval(
+        () => this.increment(),
+        1000
+      );
       this.setState({
         isPaused: false,
-        timer: this.state.timer + 1
+        timer: this.state.timer
       });
     } else {
+      clearInterval(this.timerId);
       this.setState({
         isPaused: true,
+        timer: this.state.timer
+      });
+    }
+  }
+
+  resetTimer() {
+    if (this.state.isPaused) {
+      this.setState({
         timer: 0
       });
     }
   }
 
-  render() {
-    if (!this.state.isPaused) {
-      return (
-        <div>
-          <div className={'circle'}>
-            <p>{this.state.timer}</p>
-          </div>
-          <i className={'fas fa-pause'} onClick={this.handlePlay}></i>
-        </div>
-      );
+  determineButton() {
+    if (this.state.isPaused) {
+      return 'fas fa-play';
     }
+    return 'fas fa-pause';
+  }
+
+  render() {
     return (
       <div>
-        <div className={'circle'}>
-          <p>{this.state.timer}</p>
+        <div className={'watch'}>
+          <p onClick={this.resetTimer}>{this.state.timer}</p>
         </div>
-        <i className={'fas fa-play'} onClick={this.handlePlay}></i>
+        <i className={this.determineButton()} onClick={this.handlePlay}></i>
       </div>
     );
   }
